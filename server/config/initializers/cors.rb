feb129d77662fcb386cf01c153a909d616243ce6
github username <github email address>
2019-02-13 20:41:54 +0000

    encoded API

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'https://crypto--bot.herokuapp.com'
 
     resource '*',
       headers: :any,
