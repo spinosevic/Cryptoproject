188b95edbbb3dcb3ccd3af6cd593ade8f147e646
github username <github email address>
2019-02-14 22:22:38 +0000

    Functions in appliction controller

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -25,7 +25,50 @@ class ApplicationController < ActionController::API
     end
   end
 
+  def ticker_results(market)
+    url_ticker ="https://bittrex.com/api/v1.1//public/getticker"
+    ticker_response=  HTTParty.get(
+                      url_ticker,
+                      query: {
+                      market: market
+                                  }
+                      )
+    return ticker_response["result"]
+  end
 
+  def orderbook(market,coin,action)
+    url_orderbook="https://api.bittrex.com/api/v1.1/public/getorderbook?market=BTC-#{coin}&type=#{action}"
+    orderbook_response=  HTTParty.get(
+      url_orderbook,
+      query: {
+      market: market
+     }
+      )
+      return orderbook_response["result"]
+  end
+
+
+  def market_action(api_key,api_secret,coin,balance_amount,rate,action)
+    url= "https://api.bittrex.com/api/v1.1/market/#{action}limit?apikey=#{api_key}&market=BTC-#{bot.coin}&quantity=#{balance_amount}&rate=#{rate}&nonce=#{Time.now.to_i}"
+    sign= OpenSSL::HMAC.hexdigest('sha512', api_secret.encode("ASCII"),url.encode("ASCII"))
+    return response = HTTParty.get(
+    url,
+    headers: {
+      apisign: sign
+    }
+  )
+  end
+  def get_balance(api_key,api_secret,coin)
+    url_balance ="https://api.bittrex.com/api/v1.1/account/getbalance?apikey=#{api_key}&currency=#{coin}&nonce=#{Time.now.to_i}"
+    sign_balance=OpenSSL::HMAC.hexdigest('sha512', api_secret.encode("ASCII"),url_balance.encode("ASCII"))
+    balance_response = HTTParty.get(
+    url_balance,
+    headers: {
+      apisign: sign_balance
+    }
+  )
+  return balance_response['result']['Balance']
+end
   def secret
     ENV["ENCODE_SECRET"]
   end
--- server/app/workers/test_worker.rb
+++ server/app/workers/test_worker.rb
@@ -10,14 +10,8 @@ class TestWorker < ApplicationController
       last_operation=bot.middle_point
       market="BTC-#{bot.coin}"
       ticker_price=0
-      url_ticker ="https://bittrex.com/api/v1.1//public/getticker"
-      ticker_response=  HTTParty.get(
-                        url_ticker,
-                        query: {
-                        market: market
-                                    }
-                        )
-      ticker_results=ticker_response["result"]
+
+      ticker_results=ticker_results(market)
         if bot.selling_mode==true
           ticker_price=ticker_results["Bid"]
         else
@@ -29,76 +23,47 @@ class TestWorker < ApplicationController
       puts "percentage: #{percentage}"
 
 
-              if bot.selling_mode==true
-                # variation>0 && variation.abs>=percentage &&
+              if variation>0 && variation.abs>=percentage && bot.selling_mode==true
+
                 puts "SELLING MODE"
-                url_balance ="https://api.bittrex.com/api/v1.1/account/getbalance?apikey=#{api_key}&currency=#{bot.coin}&nonce=#{Time.now.to_i}"
-                sign_balance=OpenSSL::HMAC.hexdigest('sha512', api_secret.encode("ASCII"),url_balance.encode("ASCII"))
-                balance_response = HTTParty.get(
-                url_balance,
-                headers: {
-                  apisign: sign_balance
-                }
-              )
-              balance=balance_response['result']['Balance']
+              balance=get_balance(api_key,api_secret,bot.coin)
               puts "balance: #{balance}"
 
               balance_amount=balance*bot.ratio
               puts "balance_amount: #{balance_amount}"
 
-              url_orderbook="https://api.bittrex.com/api/v1.1/public/getorderbook?market=BTC-#{bot.coin}&type=buy"
-              orderbook_response=  HTTParty.get(
-                                url_orderbook,
-                                query: {
-                                market: market
-                                            }
-                                )
-              best_deal=orderbook_response['result'].sort_by{|x| -x["Rate"]}.select{|y| y["Quantity"]>balance_amount}[0]
+              best_deal=orderbook(market,bot.coin,"buy").sort_by{|x| -x["Rate"]}.select{|y| y["Quantity"]>balance_amount}[0]
               puts "best deal: #{best_deal}"
-            #   url_sell = "https://api.bittrex.com/api/v1.1/market/selllimit?apikey=#{api_key}&market=BTC-#{bot.coin}&quantity=#{balance_amount}&rate=#{best_deal["Rate"]}&nonce=#{Time.now.to_i}"
-            #   sign_sell = OpenSSL::HMAC.hexdigest('sha512', api_secret.encode("ASCII"),url_sell.encode("ASCII"))
-            #   sell_response = HTTParty.get(
-            #   url_sell,
-            #   headers: {
-            #     apisign: sign_sell
-            #   }
-            # )
-            # if sell_response["success"]==true
+            sell_response=market_action(api_key,api_secret,bot.coin,balance_amount,best_deal["Rate"],"sell")
+            if sell_response["success"]==true
+              new_balance=balance
+              while balance==new_balance
+                new_balance=get_balance(api_key,api_secret,bot.coin)
+              end
               last_operation = best_deal["Rate"]
               bot.middle_point=last_operation
               bot.selling_mode = !bot.selling_mode
               bot.gained_amount = best_deal["Rate"]*balance_amount
               bot.save
-            # end
-          elsif bot.selling_mode==false
-                # variation<0 && variation.abs>=percentage &&
+            end
+          elsif variation<0 && variation.abs>=percentage && bot.selling_mode==false
+
                 puts "BUYING MODE"
-                url_orderbook="https://api.bittrex.com/api/v1.1/public/getorderbook?market=BTC-#{bot.coin}&type=sell"
-                orderbook_response=  HTTParty.get(
-                                  url_orderbook,
-                                  query: {
-                                  market: market
-                                              }
-                                  )
-                best_deal=orderbook_response['result'].sort_by{|x| x["Rate"]}.select{|y| y["Quantity"]>=(bot.gained_amount/y["Rate"])}[0]
+              buy_balance=get_balance(api_key,api_secret,bot.coin)
+                best_deal=orderbook(market,bot.coin,"sell").sort_by{|x| x["Rate"]}.select{|y| y["Quantity"]>=(bot.gained_amount/y["Rate"])}[0]
                 puts "best deal: #{best_deal}"
                 balance_amount=bot.gained_amount/best_deal["Rate"]
-
-              #   url_buy = "https://api.bittrex.com/api/v1.1/market/buylimit?apikey=#{api_key}&market=BTC-#{bot.coin}&quantity=#{balance_amount}&rate=#{best_deal["Rate"]}&nonce=#{Time.now.to_i}"
-              #   sign_buy = OpenSSL::HMAC.hexdigest('sha512', api_secret.encode("ASCII"),url_buy.encode("ASCII"))
-              #   buy_response = HTTParty.get(
-              #   url_buy,
-              #   headers: {
-              #     apisign: sign_sell
-              #   }
-              # )
-              # if buy_response["success"]==true
+                buy_response=market_action(api_key,api_secret,bot.coin,balance_amount,best_deal["Rate"],"buy")
+              if buy_response["success"]==true
+                new_buy_balance=balance
+                while balance=new_buy_balance
+                new_buy_balance=get_balance(api_key,api_secret,bot.coin)
+                end
                 last_operation = best_deal["Rate"]
                 bot.middle_point=last_operation
-
                 bot.selling_mode = !bot.selling_mode
                 bot.save
-              # end
+              end
             end
 
     }
